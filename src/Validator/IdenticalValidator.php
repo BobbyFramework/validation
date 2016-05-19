6c2c3b81cad19388a706a142d1b49bdd9d9517be
nicolas <n.wallerand@gmail.com>
2016-05-19 16:49:50 +0200

    Update IdenticalValidator.php

--- src/Validator/IdenticalValidator.php
+++ src/Validator/IdenticalValidator.php
@@ -2,10 +2,11 @@
 namespace BobbyFramework\Validation\Validator;
 
 use BobbyFramework\Validation\Validator;
+use BobbyFramework\Validation\Validation;
 
 class IdenticalValidator extends Validator {
 
-    public function isValid($value)
+    public function isValid(Validation $validation,$value)
     {
         return false;
     }
