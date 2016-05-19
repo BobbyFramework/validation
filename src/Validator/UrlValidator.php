5c348c2d61f794a3ca389ca6fdee7ad870f1c782
nicolas <n.wallerand@gmail.com>
2016-05-19 16:51:56 +0200

    Update UrlValidator.php

--- src/Validator/UrlValidator.php
+++ src/Validator/UrlValidator.php
@@ -2,10 +2,11 @@
 namespace BobbyFramework\Validation\Validator;
 
 use BobbyFramework\Validation\Validator;
+use BobbyFramework\Validation\Validation;
 
 class UrlValidator extends Validator {
 
-    public function isValid($value)
+    public function isValid(Validation $validation,$value)
     {
         return false;
     }
