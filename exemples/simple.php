b4f6f0b7c3d113e96c2488fcbfef7879414316bb
nicolas <n.wallerand@gmail.com>
2016-05-27 09:01:57 +0200

    Update simple.php

--- exemples/simple.php
+++ exemples/simple.php
@@ -8,24 +8,24 @@ use BobbyFramework\Validation\Validation;
 use BobbyFramework\Validation\ValidationErrorMessages;
 use BobbyFramework\Validation\Validator\MaxLength;
 
-$data = array(
+$data = [
     'name' => 'Steve Jobs'
-);
+];
 
 $validation = new Validation(new ValidationErrorMessages([
     'MaxLength' => 'MaxLength Error  '
 ]));
 
-$validation->setValidators('name', array(
-        new MaxLength(array(
+$validation->setValidators('name', [
+        new MaxLength([
                 'maxLength' => 2
-            )
-        ),
+            ]
+        ],
     )
 );
 
 if (true === $validation->isValid($data)) {
-    echo 'valide';
+    echo 'validation is ok';
 } else {
     if ($validation->getErrorMessages()->getCount()) {
         foreach ($validation->getErrorMessages() as $message) {
