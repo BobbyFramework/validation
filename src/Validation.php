74b5616e422ca51b9153eaa6d8ce7bd9dc071085
nicolas <n.wallerand@gmail.com>
2016-08-11 21:01:07 +0200

    update validation

---
+++ src/Validation.php
@@ -0,0 +1,125 @@
+<?php
+namespace BobbyFramework\Validation;
+
+use BobbyFramework\Validation\Messages\Error as MessagesError;
+use BobbyFramework\Validation\Messages\ErrorCollection as MessagesErrorCollection;
+
+/**
+ * Class Validation
+ * @package BobbyFramework\Validation
+ */
+class Validation
+{
+    /**
+     * @var array
+     */
+    private $_validators = [];
+    /**
+     * @var MessagesErrorCollection|null
+     */
+    private $_messagesGroup = null;
+    /**
+     * @var array
+     */
+    protected $_values = [];
+    /**
+     * @var ValidationErrorMessages|null
+     */
+    protected $_ValidationErrorMessage = null;
+
+    /**
+     * Validation constructor.
+     * @param ValidationErrorMessages|null $errorMessages
+     */
+    public function __construct(ValidationErrorMessages $errorMessages = null)
+    {
+        $this->_ValidationErrorMessage = $errorMessages ?: new ValidationErrorMessages();
+        $this->_messagesGroup = new MessagesErrorCollection();
+    }
+
+    /**
+     * @return null
+     */
+    public function getValidators()
+    {
+        return $this->_validators;
+    }
+
+    /**
+     * @param $field
+     * @param array $validators
+     */
+    public function setValidators($field, $validators)
+    {
+        foreach ($validators as $validator) {
+            if ($validator instanceof Validator) {
+                $this->_validators[$field][] = $validator;
+            }
+        }
+    }
+
+    /**
+     * @param $data
+     * @return bool
+     */
+    public function isValid($data)
+    {
+        /**
+         * @var Validator $validator
+         */
+        $this->_values = $data;
+
+        $return = true;
+        foreach ($this->_validators as $fields => $field) {
+            foreach ($field as $validator) {
+                if (false === $validator->isValid($this, $fields)) {
+                    $return = false;
+                }
+            }
+        }
+
+        return $return;
+    }
+
+    /**
+     * @param MessagesError $message
+     */
+    public function appendErrorMessage(MessagesError $message)
+    {
+        $this->_messagesGroup->appendMessage($message);
+    }
+
+    /**
+     * @return MessagesErrorCollection|null
+     */
+    public function getErrorMessages()
+    {
+        return $this->_messagesGroup;
+    }
+
+    /**
+     * @return \BobbyFramework\Validation\ValidationErrorMessages
+     */
+    public function getDefaultErrorMessages()
+    {
+        return $this->_ValidationErrorMessage;
+    }
+
+    /**
+     * @param $field
+     * @param $value
+     */
+    public function setValue($field, $value)
+    {
+        $this->_values[$field] = $value;
+    }
+
+    /**
+     * @param $field
+     * @return mixed
+     */
+    public function getValue($field)
+    {
+        return $this->_values[$field];
+    }
+}
