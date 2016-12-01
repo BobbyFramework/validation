c610d21356721a56d1b6090a9182378a17b00af4
nicolas <n.wallerand@gmail.com>
2016-12-01 13:19:32 +0100

    Create Ipv6.php

---
+++ src/Validator/Ipv6.php
@@ -0,0 +1,14 @@
+<?php
+namespace BobbyFramework\Validation\Validator;
+
+use BobbyFramework\Validation\Validator;
+use BobbyFramework\Validation\Validation;
+
+class Ipv6 extends Validator
+{
+    public function isValid(Validation $validation ,$value)
+    {
+        // TODO: Implement isValid() method.
+        return false;
+    }
+}
